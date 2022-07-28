b0f7bc410fc93ef2ea19987ae7083061ef026ee4
Joe Smith <yasumoto7@gmail.com>
2022-07-28 16:30:30 -0700

    Install package dependencies for Debian/Ubuntu

--- 2022/07/run.sh
+++ 2022/07/run.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
+install_package() {
+  PACKAGE_NAME="${1}"
+
+  if ! apt list -i "${PACKAGE_NAME}" | grep -q "${PACKAGE_NAME}" 2> /dev/null; then
+      sudo apt install -y "${PACKAGE_NAME}"
+  fi
+}
+
 set -eux
 
 if [ ! -d ./.venv ]; then
   virtualenv ./.venv
 fi
+
+install_package libx11-dev
+install_package libxext-dev
 
 . ./.venv/bin/activate
 
